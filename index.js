43fa8f0e8f3d1ec4e0192f0d7605d7408f1148d5
Orlandis Cuevas <cuevassoto27@gmail.com>
2021-02-18 19:03:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(cors());
 //habilitar express.json
 app.use(express.json({extended:true }));
 //puerto de el app
-const PORT = process.env.PORT || 4000;
+const port = process.env.por || 4000;
 //Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
-app.listen(PORT, ()=>{
+app.listen(port, '0.0.0.0', ()=>{
     console.log(`El Servidor esta funcionando en el puerto ${PORT}`)
 })
